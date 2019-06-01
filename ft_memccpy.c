a7bb3200440f81cd68128421e79e4808347db02c
Mohau MOLOI <mmoloi@c4r9s8.wethinkcode.co.za>
2019-06-01 11:01:15 +0200

    adding

---
+++ ft_memccpy.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memccpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmoloi <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/01 10:19:50 by mmoloi            #+#    #+#             */
+/*   Updated: 2019/06/01 10:47:47 by mmoloi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <libft.h>
+
+void		*ft_memccpy(void *dst, const void *src, int c, size_t n)
+{
+	size_t	i;
+	char	*dst2;
+	char	*src2;
+
+	i = 0;
+	dst2 = (char *)dst;
+	src2 = (char *)src;
+
+	while (i < n)
+	{
+		dst2[i] = src2[i];
+		if ((unsigned char)src2[i] == (unsigned char)c)
+			return ((char *)dst + i + 1);
+		i++;
+	}
+	return (NULL);
+}
