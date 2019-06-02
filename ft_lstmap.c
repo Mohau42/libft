4cf177edce85636f979fc7ff5bf744fc6261c54f
Mohau MOLOI <mmoloi@c4r9s8.wethinkcode.co.za>
2019-06-02 10:14:47 +0200

    adding

---
+++ ft_lstmap.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmoloi <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/02 09:27:27 by mmoloi            #+#    #+#             */
+/*   Updated: 2019/06/02 09:43:16 by mmoloi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <libft.h>
+
+t_list		*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
+{
+	t_list *newlist;
+
+	if (lst != NULL && f != NULL)
+	{
+		newlist = f(lst);
+		if (newlist != NULL && lst->next != NULL)
+				newlist->next = ft_lstmap(lst->next, f);
+		return (newlist)
+	}
+}
